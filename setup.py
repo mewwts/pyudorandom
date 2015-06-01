66e520bf81f0dc8224564941a36cda1a3208f823
Mats Julian Olsen <mats@plysjbyen.net>
2015-06-01 16:19:50 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import pyudorandom
+
+setup(
+    name='pyudorandom',
+    version=pyudorandom.__version__,
+    packages=['pyudorandom'],
+    author=pyudorandom.__author__,
+    author_email='mats@plysjbyen.net',
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    )
+    description='Generate pseudorandom numbers by using algebra',
+    test_suite='test_pyudorandom'
+)
