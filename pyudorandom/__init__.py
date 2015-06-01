197b8926a59eeb6838c448ec876391ed7521789d
Mats Julian Olsen <mats@plysjbyen.net>
2015-06-01 16:12:41 +0200

    expose bin_gcd

--- pyudorandom/__init__.py
+++ pyudorandom/__init__.py
@@ -1,7 +1,7 @@
-from .pyudorandom import pyudorandom
+from .pyudorandom import pyudorandom, bin_gcd
 
 __title__ = 'pyudorandom'
 __version__ = '0.0.1'
 __author__ = 'Mats Julian Olsen'
 __license__ = 'MIT'
-__all__ = ['pyudorandom']
+__all__ = ['pyudorandom', 'bin_gcd']
